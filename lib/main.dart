596d2e3f1ceec3e03cbb95c0c0f89a1f7d4c9e8e
alaaRashed2001 <alaa.noman.rashed@gmail.com>
2026-09-03 11:29:35 +0300

    change import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:water_sos/core/routing/app_router.dart';
-import 'package:water_sos/water_app.dart' show WaterApp;
+import 'package:water_sos/water_app.dart';
 
 void main() {
   runApp(WaterApp(appRouter: AppRouter()));
